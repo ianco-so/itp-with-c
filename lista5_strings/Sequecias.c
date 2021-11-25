53a01aaa0f10b7b0f341ead29641750b40ad1ad9
Ianco Soares <ianco.so@gmail.com>
2021-11-24 23:12:26 -0300

    Init Sequencia.c

---
+++ lista5_strings/Sequecias.c
@@ -0,0 +1,12 @@
+//chack if a sequence of numbers has subsequences of consecutive numbers
+#include <stdio.h>
+
+#define MAX_SIZE 1000
+
+int main (void) {
+    char sequence[MAX_SIZE];
+    scanf("%s", sequence);
+    
+    //printf("%d\n", check_sequence(sequence));
+    return 0;
+}
