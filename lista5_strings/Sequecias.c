8fbde4a5895c9a78c6d5f7b07dd15a0dd8dc3d80
Ianco Soares <ianco.so@gmail.com>
2021-11-25 00:27:56 -0300

    Lista5_Sequencia Completed

--- lista5_strings/Sequecias.c
+++ lista5_strings/Sequecias.c
@@ -1,12 +1,29 @@
 //chack if a sequence of numbers has subsequences of consecutive numbers
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_SIZE 1000
+
+int check_sequence(char sequence[]) {
+    int count = 0;
+    int count2 = 0;
+    for (int i = 0; i < strlen(sequence); i++) {
+        if (sequence[i]-'0'+1 == sequence[i+1]-'0') {
+            count++;
+            if (count == 4) {
+                count2++;
+                count = 0;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    return count2;
+}
 
 int main (void) {
     char sequence[MAX_SIZE];
     scanf("%s", sequence);
-    
-    //printf("%d\n", check_sequence(sequence));
+    check_sequence(sequence) == 0 ? printf("nenhuma sequencia encontrada") : printf("%d", check_sequence(sequence));
     return 0;
 }
