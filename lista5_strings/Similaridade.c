47a44aadfbbee06ca35dbfd75cf01b8d335a6831
Ianco Soares <ianco.so@gmail.com>
2021-11-24 23:11:44 -0300

    Similaridade completed

--- lista5_strings/Similaridade.c
+++ lista5_strings/Similaridade.c
@@ -6,11 +6,11 @@
 
 void fill_strings(int nstr, char str[nstr][MAX_STR_LEN]){
     for (int i = 0; i < nstr; i++) {
-        fgets(str[i], MAX_STR_LEN, stdin);
+        fgets(str[i], MAX_STR_LEN+1, stdin);
         str[i][strlen(str[i]) - 1] = '\0';
     }
 }
-void handle_similarity (int nstr, char str[nstr][MAX_STR_LEN], int similarity [nstr][nstr][2]){
+void handle_similarity (int nstr, char str[nstr][MAX_STR_LEN], int similarity[nstr][nstr][2]){
     //Initialize the similarity matrix with 0
     for (int i = 0; i < nstr; i++)  for (int j = 0; j < nstr; j++)  similarity[i][j][0] = similarity[i][j][1] = 0;
     //compares strings bitwise
@@ -30,37 +30,33 @@ void handle_similarity (int nstr, char str[nstr][MAX_STR_LEN], int similarity [n
         }
     }
 }
+void print_results (int nstr, char str[nstr][MAX_STR_LEN], int similarity[nstr][nstr][2]){
+    for (int i = 0; i < nstr+1; i++) {
+        for (int j = 0; j < nstr+1; j++) {
+            if (i == 0 && j == 0) for (int k = 0; k < MAX_STR_LEN; k++) printf(" ");
+            else if (i == 0) {
+                for (int k = strlen(str[j-1]); k < MAX_STR_LEN; k++)    printf(" ");
+                for (int k = 0; str[j-1][k] != '\0'; k++)   printf("%c", str[j-1][k]);
+            }
+            else if (j == 0) {
+                for (int k = 0; str[i-1][k] != '\0'; k++)   printf("%c", str[i-1][k]);
+                for (int k = strlen(str[i-1]); k < MAX_STR_LEN; k++)    printf(" ");
+            }
+            else {
+                for (int k = 3; k < MAX_STR_LEN; k++)   printf(" ");
+                printf("%d/%d", similarity[i-1][j-1][0], similarity[i-1][j-1][1]);
+            }
+        }
+        printf("\n");
+    }
+}
 int main (void) {
     int nstr = 0;
     scanf("%d\n", &nstr);
-
     int similarity[nstr][nstr][2];
     char str[nstr][MAX_STR_LEN];
     fill_strings(nstr, str);
-
     handle_similarity(nstr, str, similarity);
-
-    //Prints the similarity matrix
-    char strspace[MAX_STR_LEN];
-    for (int i = 0; i < MAX_STR_LEN-1; i++) strspace[i] = ' ';
-    strspace[MAX_STR_LEN-1] = '\0';
-    for (int i = 0; i < nstr+1; i++) {
-        for (int j = 0; j < nstr+1; j++) {
-            if (i == 0 && j == 0) printf("%s", strspace);
-            else if (i == 0) printf("%s%s", str[j-1],strspace);
-            else if (j == 0) printf("%s%s", str[i-1],strspace);
-            else printf("%d/%d%s", similarity[i-1][j-1][0], similarity[i-1][j-1][1], strspace);
-        }
-        printf("\n");
-    }
-    /*
-    for (int i = 0; i < nstr; i++) {
-        for (int j = 0; j < nstr; j++) {
-            printf("%d/%d ", similarity[i][j][0], similarity[i][j][1]);
-        }
-        printf("\n");
-    }
-    */
-
+    print_results(nstr, str, similarity);
     return 0;
 }
