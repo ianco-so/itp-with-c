15bc671367f1e99ceafe1d247716e77d7bff3453
Ianco Soares <ianco.so@gmail.com>
2021-11-25 22:30:16 -0300

    Lista5_Frequencia completed

--- lista5_strings/Frequencia.c
+++ lista5_strings/Frequencia.c
@@ -1,43 +1,48 @@
-//count the number of times the given string appears in the phrase and divide by the number of words in the phrase
-//calculate the frequency and print it
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE 255
+#define MAX_SIZE 256
+
+//counts how many times a string is substring of another string
+int count_substring(char *str, char *sub){
+    int cnt = 0;
+    char *p;
+    while (p = strstr(str, sub)){
+        str = p + 1;
+        ++cnt;
+    }
+    return cnt;
+}
+
+//count the number of words in the each entry
+int count_words(char *str){
+    int cnt = 0;
+    for (int i = 0; i <= strlen(str); i++) {
+        if (str[i] == ' ' || str[i] == '\0')  cnt++;
+    }
+    return cnt;
+}
 
 int main (void) {
-    char reference_word[MAX_SIZE];
+    char ref_word[MAX_SIZE];
     char phrase[MAX_SIZE] = {'\0'};
-    int count_words = 0, count_occurrences = 0, count_words_total = 0, count_occurrences_total = 0;
+    int cnt_words = 0, cnt_occurrences = 0, cnt_words_total = 0, cnt_occurrences_total = 0;
     //Read the reference word
-    scanf("%s", reference_word);
+    scanf("%s", ref_word);
     getchar();
     while (strcmp(phrase, "-1") != 0){
         //Read, with a while loop, the nexts entries
         fgets(phrase, MAX_SIZE, stdin);
         phrase[strlen(phrase) - 1] = '\0';
         if (strcmp(phrase, "-1") == 0) break;
-        //count the number of words in the each entry
-        for (int i = 0; i <= strlen(phrase); i++) {
-            if (phrase[i] == ' ' || phrase[i] == '\0') {
-                count_words++;
-            }
-        }
-
-        for (int i = strlen(phrase) - strlen(strstr(phrase, reference_word)); i < strlen(phrase); i++) {
-            //count the number of occurrences of the reference word in the entry
-            if (strstr(phrase, reference_word) != NULL) {
-                count_occurrences++;
-            }
-        }
-        printf("%d\n", count_occurrences);
+        cnt_words = count_words(phrase);
+        cnt_occurrences = count_substring(phrase, ref_word);
+        printf("%.4f\n", cnt_occurrences / (float)cnt_words);
         //count the total number of words in all entries
-        count_words_total = count_words_total + count_words;
-        count_words = 0;
-        //count the total number of times the given string appears in all phrases and the number of words in all phrases
-        count_occurrences_total = count_occurrences_total + count_occurrences;
-        count_occurrences = 0;
+        cnt_words_total += cnt_words;
+        //count the total number of times the given string appears in all phrases
+        cnt_occurrences_total += cnt_occurrences;
     }
-    printf("%d, %d, %d", count_words, count_words_total, count_occurrences_total);
+    printf("%.4f\n", cnt_occurrences_total / (float)cnt_words_total);
     return 0;
 }
