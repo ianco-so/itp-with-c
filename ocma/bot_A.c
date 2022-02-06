beaca7bb2a60104222327eb79968a168926343e0
Ianco Soares <ianco.so@gmail.com>
2022-02-06 00:08:34 -0300

    Leitura de dados e achar ponto de pesca mais próximo completos

--- ocma/bot_A.c
+++ ocma/bot_A.c
@@ -1,115 +1,151 @@
 /********************************************************************
-  Bot-exemplo
+bot_A
   
-  Após receber as informações iniciais do jogo, a cada rodada esse
-  bot irá se movimentar para esquerda.
-  Cabe a você agora aprimorar sua estratégia!!!
- ********************************************************************/
+After receiving the initial information of the game, each round this
+bot will find the best action to take and send it to the ocma.
+The goal of this bot is to maximize the fishing profit keeping 
+the environment safe
+********************************************************************/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define MAX_STR 50
+//MULLET (Tainha): R$ 100.00/Kg (10 <= x < 20)
+//SNAPPER (Cioba): R$ 150.00/Kg (20 <= x < 30)
+//SEABASS (Robalo): R$ 200.00/Kg (30 <= x < 40)
 
-typedef struct POSICAO{
+
+typedef struct POSITION {
   int x, y;
-} Posicao;
+} Position;
 
-// Estrutura dos bots
-typedef struct Player {
+typedef struct PLAYER {
   char id[MAX_STR];
-  Posicao posicao;
+  Position position;
 } Bot;
-
-/* ADAPTAR EM FUNÇÃO DE COMO OS DADOS SERÃO ARMAZENADOS NO SEU BOT */
-Bot * readData(int h, int w, int areaDePesca[h][w], int *nBots) {
+ 
+Bot * readData(int h, int w, int fishingArea[h][w], int *nBots) {
   char id[MAX_STR];
   int n;
 
-  // lê os dados da área de pesca
+  // read the fishing area
   for (int i = 0; i < h; i++) {   
     for (int j = 0; j < w; j++) {
-      scanf("%i", &areaDePesca[i][j]);
+      scanf("%i", &fishingArea[i][j]);
     }
   }
-  // lê os dados dos bots
-  scanf(" BOTS %i", &n);
-  *nBots = n;
-  // o " " antes de BOTS é necessário para ler o '\n' da linha anterior
+
+  // read the data of the players
+  scanf(" BOTS %i", &n); // The ' ' is for the '\n' of the previous line
+  *nBots = n; 
   Bot *bots = malloc(n * sizeof(Bot));
   for (int i = 0; i < n; i++) {
-    scanf("%s %i %i", bots[i].id, &bots[i].posicao.x, &bots[i].posicao.y); // lê o id dos bots e suas posições
+    scanf("%s %i %i", bots[i].id, &bots[i].position.x, &bots[i].position.y);
   }
   return bots;
 }
 
-int main() {
-  char line[MAX_STR];   // dados temporários
-  char myId[MAX_STR];   // identificador do bot em questão
-  int nBots;            // quantidade de bots
-  Bot *bots;            // vetor de bots
-  Bot myBot;            // bot em questão
-  Posicao posicaoPassada; // posição anterior do bot
+int priceOfFish(int x) { // return the price of the fish x
+  if (x == 1) {
+    return 100;
+  } else if (x == 2) {
+    return 150;
+  } else if (x == 3) {
+    return 200;
+  } else {
+    return 0;
+  }
+}
+Position findCloserFishingSpot (int h, int w, int nEnemies, int fishingArea[h][w], Bot enemies[nEnemies], Position currentPosition) {
+  Position closerFishingSpot = currentPosition;
+  int copiedFishingArea[h][w];
 
-  setbuf(stdin, NULL);   // stdin, stdout e stderr não terão buffers
-  setbuf(stdout, NULL);  // assim, nada é "guardado temporariamente"
+  //copy the fishing area
+  for(int i = 0; i < h; i++) {
+    for(int j = 0; j < w; j++) {
+      copiedFishingArea[i][j] = fishingArea[i][j];
+    }
+  }
+  //Overlap the copied fishing area with enemies positions
+  for (int i = 0; i < nEnemies; i++) {
+    copiedFishingArea[enemies[i].position.x][enemies[i].position.y] = -1;
+  }
+  //Overlap the copied fishing area with the current position
+  copiedFishingArea[currentPosition.x][currentPosition.y] = fishingArea[currentPosition.x][currentPosition.y];
+
+  //Find the closer fishing spot
+  int distance = 999999999; //arbitrary big number
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      //If the position is a fishing spot
+      if (copiedFishingArea[i][j] > 11 && copiedFishingArea[i][j]%10 > 1) {
+        //calculate the distance from the current position
+        if (abs(currentPosition.x - i) + abs(currentPosition.y - j) < distance) { //If the distance of this fishing spot is smaller than the previous one
+          distance = abs(currentPosition.x - i) + abs(currentPosition.y - j);
+          //Save the closer fishing spot
+          closerFishingSpot.x = i;
+          closerFishingSpot.y = j;
+        } else if (distance == abs(currentPosition.x - i) + abs(currentPosition.y - j)) { //If the distance of this fishing spot is equal to the previous one
+          //find the better cost-benefit ratio
+          int currentCostBenefitRatio = priceOfFish(copiedFishingArea[i][j]/10)* (copiedFishingArea[i][j]%10);
+          int previousCostBenefitRatio = priceOfFish(copiedFishingArea[closerFishingSpot.x][closerFishingSpot.y]/10) * (copiedFishingArea[closerFishingSpot.x][closerFishingSpot.y]%10);
+          if (currentCostBenefitRatio > previousCostBenefitRatio) {
+            //Save the closer fishing spot with the better cost-benefit ratio
+            closerFishingSpot.x = i;
+            closerFishingSpot.y = j;
+          }
+        }
+      }
+    }
+  }
+  return closerFishingSpot; //if there is no closer fishing spot, the current position will be returned
+}
+
+int main() {
+  char line[MAX_STR];   // temporarily data
+  //char myId[MAX_STR]; 
+  int nBots;        
+  Bot *bots, myBot;
+  Position prePosition; // previous position of myBot
+
+  setbuf(stdin, NULL);   // stdin, stdout and stderr are unbuffered, so nothing is keeping momentarily
+  setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
-  // === INÍCIO DA PARTIDA ===
-  int h, w;
-  scanf("AREA %i %i", &h, &w);  // lê a dimensão da área de pesca: altura (h) x largura (w)
-  int areaDePesca[h][w];
-  scanf(" ID %s", myBot.id);        // ...e o id do bot
-  // obs: o " " antes de ID é necessário para ler o '\n' da linha anterior
+  // === START OF THE GAME ===
+  int h, w; // height and width of the fishing area
+  scanf("AREA %i %i", &h, &w);
+  int fishingArea[h][w];
+  scanf(" ID %s", myBot.id); // the ' ' is necessary to read the '\n' of the previous line
 
-  // Para "debugar", é possível enviar dados para a saída de erro padrão (stderr).
-  // Esse dado não será enviado para o simulador, apenas para o terminal.
-  // A linha seguinte é um exemplo. Pode removê-la se desejar.
-  //fprintf(stderr, "Meu id = %s\n", myId);
 
-  // === PARTIDA === 
-  // O bot entra num laço infinito, mas não se preocupe porque o simulador irá matar
-  // o processo quando o jogo terminar.
+  // use fprintf(strerr, "STUFF", var) for debug if necessary. Ex: fprintf(stderr, "Meu id = %s\n", myId);
+
+  // === TURNS === 
   while (1) {
-    memset(areaDePesca, 0, sizeof(areaDePesca)); // zera a área de pesca
+    memset(fishingArea, 0, sizeof(fishingArea)); // zera a área de pesca
 
     // LÊ OS DADOS DO JOGO E ATUALIZA OS DADOS DO BOT
-    bots = readData(h, w, areaDePesca, &nBots);
+    bots = readData(h, w, fishingArea, &nBots);
 
-    // guarda a posição anterior do bot
-    posicaoPassada = myBot.posicao;
-    //fprintf(stderr, "Posição anterior: %i %i\n", posicaoPassada.x, posicaoPassada.y);
-    // procura meu bot na lista de bots
+    prePosition = myBot.position;
+
+    // find myBot in the list of bots (mainly the position)
     for (int i = 0; i < nBots; i++) {
       if (strcmp(bots[i].id, myBot.id) == 0) {
         strcpy(myBot.id, bots[i].id);
-        myBot.posicao = bots[i].posicao;
+        myBot.position = bots[i].position;
         break;
       }
     }
-    //fprintf(stderr, "My bot: %s %i %i\n", myBot.id, myBot.posicao.x, myBot.posicao.y);
-    /*
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-        fprintf(stderr, "%i ", areaDePesca[i][j]);
-      }
-      fprintf(stderr, "\n");
-    }
-    */
-    /*
-    fprintf(stderr, "Tem %i bots, sao: ", nBots);
-    for (int i = 0; i < nBots; i++) {
-      fprintf(stderr, "%s %d %d, ", bots[i].id, bots[i].posicao.x, bots[i].posicao.y);
-    }
-    fprintf(stderr, "\n");
-    */
-    // INSIRA UMA LÓGICA PARA ESCOLHER UMA AÇÃO A SER EXECUTADA
-
-    // envia a ação escolhida (nesse exemplo, ir para esquerda)
+    // insert a logic to be executed each turn
+    Position goalPosition = findCloserFishingSpot(h, w, nBots, fishingArea, bots, myBot.position);
+    // print the action to be executed
     printf("RIGHT\n");
 
-    // lê qual foi o resultado da ação (e eventualmente atualiza os dados do bot).
+    // read the response of the action of the bot and update his status (if necessary)
     scanf("%s", line);
   }
 
